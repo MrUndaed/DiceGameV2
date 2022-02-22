5e9cd5a45a51544fff56991cd32aaea74f0fc12f
MrUndaed <62888155+MrUndaed@users.noreply.github.com>
2022-02-22 23:05:41 +0200

    ThemesUpdate

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const image = document.querySelector(".dice");
 
 const p1CurrentElement = document.getElementById("current--0");
 const p2CurrentElement  = document.getElementById("current--1");
+const current = document.querySelectorAll(".current");
 
 const p1Total = document.getElementById("score--0");
 const p2Total = document.getElementById("score--1");
+const score = document.querySelectorAll("score");
 
 const player0 = document.querySelector('.player--0');
 const player1 = document.querySelector('.player--1');
@@ -14,6 +16,11 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const diceEl = document.querySelector('.dice');
+
+const body = document.querySelector(".body");
+const themes = document.querySelector(".themes");
+const main = document.querySelector(".main");
+const themeTab = document.querySelector(".theme-tab");
 
 let currentScore, playing;
 
@@ -137,3 +144,44 @@ btnHold.addEventListener("click", function(){
 //---MAKE NEW GAME BTN WORK---
 
 btnNew.addEventListener("click", function(){init()});
+
+
+// ---- THEMES ----
+
+document.querySelector(".purple").addEventListener("click", function(){
+body.classList.add("purple-background-theme");
+console.log(current.length);
+p1Total.classList.add("purple-text");
+p2Total.classList.add("purple-text");
+
+for(let i = 0; i <current.length; i++){
+current[i].classList.add("purple-theme");
+console.log("AAAAAAAAAAAAA");
+}
+});
+
+document.querySelector(".clasic").addEventListener("click", function(){
+body.classList.remove("purple-background-theme");
+p1Total.classList.remove("purple-text");
+p2Total.classList.remove("purple-text");
+
+for(let i = 0; i <current.length; i++){
+current[i].classList.remove("purple-theme");
+}
+});
+
+document.addEventListener("keydown", function(event){
+  console.log(event.key); //this will log the key that was pressed
+  if(event.key == "Tab"){
+    /*
+    const body = document.querySelector(".body");
+    const themes = document.querySelector(".themes");
+    const main = document.querySelector(".main");
+    const themeTab = document.querySelector(".theme-tab");
+    */
+    themes.classList.toggle("hidden");
+    main.classList.toggle("hidden");
+    themeTab.classList.toggle("hidden");
+
+  }
+});
